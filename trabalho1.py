38462472dc81f6afcdb81092566eab532a9eebe2
milenemathias <milene.mathiass@gmail.com>
2024-04-22 13:13:10 -0300

    first commit

---
+++ trabalho1.py
@@ -0,0 +1,9 @@
+# Entrada de dados do usuário
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+
+# Conversão para Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Exibição do resultado
+print("A temperatura em Fahrenheit é:", fahrenheit)
+
